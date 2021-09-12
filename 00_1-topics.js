9843a908475edb9d2554d94d66dc62d002df8876
Amit Gupta <33304344+Am1tgupt4@users.noreply.github.com>
2021-09-13 02:10:10 +0530

    Topics added

---
+++ 00_1-topics.js
@@ -0,0 +1,110 @@
+// Topics
+
+
+/*
+ 1. Big O Notation
+ 2. Analyzing Array and Objects
+ 3. Problem Solving Approach
+ 4. Problem Solving Patterns
+ 5. Recursion
+ 6. Searching Algorithms 
+ 7. Bubble Sort
+ 8. Selection Sort
+ 9. Insertion Sort
+ 10. Merge Sort
+ 11. Quick Sort
+ 12. Radix Sort
+ 
+ 
+ 1.Intro To Data Structure
+ 2. Singly Linked List
+ 3. Doubly Linked List
+ 4. Stacks and Queues
+ 5. BTS (Binary Search Trees)
+ 6. Tree Traversals
+ 7. Binary Heaps
+ 8. Hash Tables
+ 9. Graphs
+ 10. Graphs Traversal
+ 11. Dijesktra Algorithm
+*/
+
+
+// https://www.youtube.com/watch?v=lDTKnzrX6qU&ab_channel=ChrisJereza 
+Iteration + Recursion
+Arrays + Matrices(Multi dimensional Arrays)
+Linked Lists
+Queues + Stacks + Heaps
+Sets + Hash Maps
+Trees + Binary Search
+Graphs + DFS + BFS + Backtracking
+Tries 
+Sorting Algorithms: Merge Sort, Quick Sort
+(Memorise time and space complexity)
+
+Dynamic Programming :
+Memoization
+Divide and Conquer 
+
+<Practice Proofs>:
+Contrapositive
+Contradiction
+Cases
+Induction 
+
+
+Logical/Mathematical proof techniques:
+Sliding Window
+Multiple Pointers
+Greedy Algorithms
+Object Oriented Programming (Clean Code by Robert Martin, especially the sections on Single Responsibility Principle)
+
+
+Most useful DataStructure:
+Hash maps
+Stacks and Queues 
+Sets and DFS
+
+
+Resources:
+
+Practice from here:
+Leetcode: Questions High like:dislike ratio
+Questions: (Data structure) Hackerrank and GeeksofGeeks
+Cracking the Coding Interview + Video made by Author
+
+Be Experienced
+- Experience means Rate at you observe key lessons from things + usefulness of these lessons.
+- Proactively look for patterns, write it down.
+- Practice as realistically as possible.
+- Do a lot of interviews + Mock Interviews.
+You know what you’re doing
+- Rely on habits, rules and system.
+- Ex: When interviewer ended explaining the question, what things you should do?
+    - Description of the problem
+    - Sample Input and output
+    - Constraints
+    - Whiteboard/shared screen
+
+- Create a drawing in your head
+- Ask few questions:
+    - Why it is hard to code
+    - What assumption can I make to make this problem easier
+    - What is my big o runtime
+    - Whats next best big o
+    - Why is this possible?
+    - Is it not possible?
+    - Where the repeated work happening?
+    - Does this need to happen?
+Explain your solution to your interviewer:
+- Explain solution in plain English
+- Step by step (bullets)
+- Pseudocode
+- Functions
+- Implement
+
+Be Smart
+- Creativity (Asking questions, Making relevant assumptions of problem space)
+- Self Awareness (When you’re going down to wrong path and correcting it)
+- Consistency (Do everyday)
+- Intensity (Label of mental Engagement)
